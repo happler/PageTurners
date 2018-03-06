55c63c4feeae67046789854f4b558d9becfe21ce
Happler <harry.appler@gmail.com>
2018-03-06 17:12:43 -0500

    update GEMFILE

--- frontend/page_turners.jsx
+++ frontend/page_turners.jsx
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", () =>{
   } else{
     store = configureStore();
   }
+  window.store = store;
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
 });
