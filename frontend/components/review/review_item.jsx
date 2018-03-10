aee755b21e1caecad13bffc1f5c41208df88a769
Happler <harry.appler@gmail.com>
2018-03-09 23:54:15 -0500

    start on review item

--- frontend/components/review/review_item.jsx
+++ frontend/components/review/review_item.jsx
@@ -3,7 +3,19 @@ import React from 'react';
 const ReviewItem =>(
   return(
     <article className='review-item-container'>
-      <header className='review-item__user'
+      <header className='review-item__user'>
+       <img className='review-item__user__image' src={this.props.userImage} />
+       <div className='review-item__user__info'>
+         <p className='review-item__user__name'>{this.props.username}</p>
+         <ul>
+           <li className='review-item__1star'></li>
+           <li className='review-item__2star'></li>
+           <li className='review-item__3star'></li>
+           <li className='review-item__4star'></li>
+           <li className='review-item__5star'></li>
+         </ul>
+       </div>
+      </header>
 
     </article>
   )
