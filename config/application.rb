d61184356fdee208621a257802e1c04764c6b4e4
Happler <harry.appler@gmail.com>
2018-03-12 17:12:32 -0400

    add config file to production

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module PageTurners
       :s3_credentials => {
       :bucket => ENV["s3_bucket"],
       :access_key_id => ENV["s3_access_key_id"],
-      :secret_access_key_id => ENV["s3_secret_access_key_id"],
+      :secret_access_key => ENV["s3_secret_access_key"],
       :s3_region => ENV["s3_region"]
   }
 }
