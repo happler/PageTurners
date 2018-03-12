8a79b247fff5bcf53daf5c304bf309c38a23bc43
Happler <harry.appler@gmail.com>
2018-03-12 17:25:24 -0400

    change prod again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_region => 'us-east-1',
     :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
+      :bucket => ENV['s3_bucket'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }}
