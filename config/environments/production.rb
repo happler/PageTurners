f18a687104a73930f71647351a6944f2b5d9eeec
Happler <harry.appler@gmail.com>
2018-03-12 17:18:41 -0400

    change lkajsdl;fh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_region => ENV['AWS_REGION'],
+    :s3_region => 'us-east-1',
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
