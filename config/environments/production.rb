b54485307b92ff5242a8639fe99afa352096497e
Happler <harry.appler@gmail.com>
2018-03-12 17:14:21 -0400

    add closing curley

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,5 +97,5 @@ Rails.application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
+  }}
 end
