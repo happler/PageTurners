f89f620eb4f2e26a25138b1a97fb8a3cc4771e5d
Happler <harry.appler@gmail.com>
2018-03-13 14:09:55 -0400

    change config pointer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  # config.paperclip_defaults = {
-  #   :storage => :s3,
-  #   :s3_region => ENV['s3_region'],
-  #   :s3_credentials => {
-  #     :bucket => ENV['s3_bucket'],
-  #     :access_key_id => ENV['s3_access_key_id'],
-  #     :secret_access_key => ENV['s3_secret_access_key']
-  # }}
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_region => ENV['AWS_REGION'],
+    :s3_credentials => {
+      :bucket => ENV['s3_bucket'],
+      :access_key_id => ENV['s3_access_key_id'],
+      :secret_access_key => ENV['s3_secret_access_key']
+  }}
 end
