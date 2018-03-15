208ca58bef242e9f34ed62ce423337a158a78204
Happler <harry.appler@gmail.com>
2018-03-15 14:01:01 -0400

    add links to book pages on shelves

--- frontend/components/bookshelf/bookshelf_item.jsx
+++ frontend/components/bookshelf/bookshelf_item.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import MiniReviewContainer from '../review/mini_review_container';
 
-const BookshelfItem = ({book}) =>{
+const BookshelfItem = ({book, history}) =>{
   const { id, title, author, coverImage, avgReview, reviewCount, currentUserReview} = book;
 
   const updatedRating = currentUserReview[0]
     ? (((avgReview * reviewCount) + currentUserReview[0].rating) / (reviewCount + 1)).toFixed(3)
     : (avgReview).toFixed(3);
 
+    const toBookPage = () => history.push(`/books/${id}`);
+
     return(
       <tr>
-        <td><img className='bookshelf-item__image' src={ coverImage }></img></td>
-        <td className='bookshelf-item__title' >{title}</td>
+        <td><img onClick={toBookPage} className='bookshelf-item__image' src={ coverImage }></img></td>
+        <td onClick={toBookPage} className='bookshelf-item__title' >{title}</td>
         <td className='bookshelf-item__author' >{author}</td>
         <td className='bookshelf-item__avgRating' >{updatedRating}</td>
         <td className='bookshelf-item__mini_review' ><MiniReviewContainer reviews={currentUserReview} bookId={id} /></td>
