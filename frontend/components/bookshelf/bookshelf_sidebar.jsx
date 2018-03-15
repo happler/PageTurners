3adc1732c795737aa9d86422f4740a9add54c72d
Happler <harry.appler@gmail.com>
2018-03-15 16:29:41 -0400

    add bookshelf delete, and protect against deleting initial shelves

--- frontend/components/bookshelf/bookshelf_sidebar.jsx
+++ frontend/components/bookshelf/bookshelf_sidebar.jsx
@@ -7,6 +7,7 @@ class BookshelfSidebar extends React.Component{
     this.state={title:''};
     this.update = this.update.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.destroy = this.destroy.bind(this);
   }
 
   componentDidMount(){
@@ -19,12 +20,26 @@ class BookshelfSidebar extends React.Component{
     this.setState({title:e.currentTarget.value});
   }
 
+  destroy(id){
+    return e => this.props.deleteShelf(id);
+  }
+
   handleSubmit(e){
     e.preventDefault();
     this.props.postShelf(this.state).then(() => this.setState({title:''}) );
   }
 
   render(){
+
+    const shelfLinks = this.props.userShelves.map((shelf, idx) => (
+      <div key={idx}>
+        <Link to={`/users/${this.props.match.params.userId}/shelf/${shelf.id}`} key={idx}>{shelf.title}</Link>
+        {(["Read", "Reading", "Want to Read"].indexOf(shelf.title) === -1 ) ?
+          <span key={idx} onClick={this.destroy(shelf.id)}>(delete)</span> : null
+        }
+      </div>
+    ));
+
     if(!this.props.userShelves){
         return(
           <div className='bookshelf-sidebar-loading-container'>
@@ -38,9 +53,7 @@ class BookshelfSidebar extends React.Component{
     return(
       <ul>
         <Link to={`/users/${this.props.match.params.userId}/shelf`} >All Shelves</Link>
-        {this.props.userShelves.map((shelf, idx) => (
-          <Link to={`/users/${this.props.match.params.userId}/shelf/${shelf.id}`} key={idx}>{shelf.title}</Link>
-        ))}
+        {shelfLinks}
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.update} value={this.state.title}></input>
           <input type='submit' value='Add Shelf'></input>
