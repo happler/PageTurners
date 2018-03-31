d664fc96127dad222cc169ec3a60ddfb1bc8f6a0
Happler <harry.appler@gmail.com>
2018-03-31 18:11:22 -0400

    the issue is when I change the rating on the review page, currently, the recieveReview action updates the currentUser Review, so the actual update average doesn't have access to the old review, it eas already deleted

--- frontend/components/book/book_image_section.jsx
+++ frontend/components/book/book_image_section.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import MiniReviewContainer from "../review/mini_review_container";
+import { reviewStats } from "../../util/review_util";
 
 const BookImageItem = ({ coverImage, reviews }) => {
   let reviewCount = 0.0001;
   let total = 0;
-
-  reviews.forEach(review => {
-    reviewCount++;
-    total += review.rating;
-  });
-  const average = total / reviewCount;
 
   return (
     <div className="book-image-container book-show__left">
